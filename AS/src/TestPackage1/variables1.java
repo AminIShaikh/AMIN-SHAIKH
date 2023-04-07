e0d3fdaf7f27dd4db1c54843c68caf34f6e6f0c1
Hp <Hp@DESKTOP-HEM8FQG>
2023-04-07 08:25:52 +0530

    CHANGES DONE FOR TEST PACKAGE 1 : VARIABLES 1

---
+++ AS/src/TestPackage1/variables1.java
@@ -0,0 +1,24 @@
+package TestPackage1;
+
+public class variables1 {
+	
+	public static void main(String[]args) {
+		
+		// variable declaration
+		String a;
+		String b;
+		String c;
+		
+		//variable initialization
+		a ="AMIN SHAIKH" ;  ///CHANGES DONE
+		b ="@" ;
+		c ="Amin @ 2499" ;
+		
+		//variables usages
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+}
+
