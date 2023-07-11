2e0b933aa2ed1d9ab23c7d5e6036082b8bb29b43
Hp <Hp@DESKTOP-HEM8FQG>
2023-07-11 13:06:49 +0530

    Done

--- AS/src/Selenium/SetSizePosition.java
+++ AS/src/Selenium/SetSizePosition.java
@@ -3,7 +3,6 @@ package Selenium;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class SetSizePosition {
--- AS/src/Selenium/ToOpenNewWindowTab.java
+++ AS/src/Selenium/ToOpenNewWindowTab.java
@@ -2,7 +2,6 @@ package Selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class ToOpenNewWindowTab {
